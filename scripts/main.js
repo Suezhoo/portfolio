7a60d2d56aaccbeae3da5a377ceac73b83a4499f
Suezhoo <suezhoo@outlook.com>
2022-02-11 02:44:36 +0100

    [WIP] Add project page
    Currently working on implementing the extending text field feature. Only "url" works at the moment

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+window.onload = function () {
+    url.addEventListener("input", resize);
+};
+const empty = document.querySelector("#empty");
+const url = document.querySelector("#url");
+
+function resize() {
+    empty.textContent = url.value;
+    url.style.width = empty.offsetWidth + "px";
+    console.log(empty.textContent, url.style.width);
+}
