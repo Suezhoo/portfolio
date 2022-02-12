71be5a84a1268cf62e3e700113a28c9d6430f74b
Suezhoo <suezhoo@outlook.com>
2022-02-12 23:16:30 +0100

    Finished project template

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 "use strict";
 
 window.onload = function () {
-    url.addEventListener("input", resize);
+    // Name
+    const emptyName = document.querySelector("#emptyName");
+    const name = document.querySelector("#name");
+    name.addEventListener("input", () => {
+        resize(emptyName, name);
+    });
+    // Description
+    const emptyDesc = document.querySelector("#emptyDesc");
+    const desc = document.querySelector("#description");
+    desc.addEventListener("input", () => {
+        resize(emptyDesc, desc);
+        autoGrow(desc);
+    });
+    // URL
+    const emptyURL = document.querySelector("#emptyURL");
+    const URL = document.querySelector("#url");
+    URL.addEventListener("input", () => {
+        resize(emptyURL, URL);
+    });
 };
-const empty = document.querySelector("#empty");
-const url = document.querySelector("#url");
 
-function resize() {
-    empty.textContent = url.value;
-    url.style.width = empty.offsetWidth + "px";
-    console.log(empty.textContent, url.style.width);
+function resize(span, input) {
+    span.textContent = input.value;
+    input.style.width = span.offsetWidth + "px";
+}
+
+function autoGrow(element) {
+    element.style.height = "5px";
+    element.style.height = element.scrollHeight + "px";
 }
